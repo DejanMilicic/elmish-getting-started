3605a06b4e0e23328a097e8109d4b6dd1e46d947
Dejan Milicic <dejan.milicic@gmail.com>
2022-04-28 18:29:46 +0200

    async http request

--- src/App.fs
+++ src/App.fs
@@ -23,53 +23,41 @@ type Request =
 
 type Response = { statusCode: int; body: string }
 
-let httpRequest (request: Request) (responseHandler: Response -> 'Msg) : Cmd<'Msg> =
-    let command (dispatch: 'Msg -> unit) =
+let httpRequest (request: Request) : Async<Response> =
+    Async.FromContinuations <| fun (resolve, reject, _) ->
         // create an instance
         let xhr = XMLHttpRequest.Create()
         // open the connection
-        xhr.``open`` (method = request.method, url = request.url)
+        xhr.``open``(method=request.method, url=request.url)
         // setup the event handler that triggers when the content is loaded
-        xhr.onreadystatechange <-
-            fun _ ->
-                if xhr.readyState = ReadyState.Done then
-                    // create the response
-                    let response =
-                        { statusCode = xhr.status
-                          body = xhr.responseText }
-                    // transform response into a message
-                    let messageToDispatch = responseHandler response
-                    dispatch messageToDispatch
+        xhr.onreadystatechange <- fun _ ->
+            if xhr.readyState = ReadyState.Done
+            then
+              // create the response
+              let response = { statusCode = xhr.status; body = xhr.responseText }
+              // transform response into a message
+              resolve response
 
         // send the request
-        xhr.send (request.body)
-
-    Cmd.ofSub command
-
-
+        xhr.send(request.body)
 
 let update msg state =
     match msg with
     | LoadLoremIpsum Started ->
         let nextState = { state with LoremIpsum = InProgress }
+        let loadLoremIpsum =
+            async {
+                let request = { url = "/lorem-ipsum.txt"; method = "GET"; body = "" }
+                let! response = httpRequest request
+                if response.statusCode = 200
+                then return LoadLoremIpsum (Finished (Ok response.body))
+                else return LoadLoremIpsum (Finished (Error "Could not load the content"))
+            }
 
-        let request =
-            { url = "/lorem-ipsum.txt"
-              method = "GET"
-              body = "" }
-
-        let responseMapper (response: Response) =
-            if response.statusCode = 200 then
-                LoadLoremIpsum(Finished(Ok response.body))
-            else
-                LoadLoremIpsum(Finished(Error "Could not load the content"))
-
-        nextState, httpRequest request responseMapper
+        nextState, Cmd.fromAsync loadLoremIpsum
 
     | LoadLoremIpsum (Finished result) ->
-        let nextState =
-            { state with LoremIpsum = Resolved result }
-
+        let nextState = { state with LoremIpsum = Resolved result }
         nextState, Cmd.none
 
 
